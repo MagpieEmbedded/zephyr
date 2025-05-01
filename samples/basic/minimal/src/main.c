61408934ca60ed1e54866ffdca0921a4594c61cd
Magpie Embedded <magpieembedded@gmail.com>
2025-05-01 21:20:44 +0100

    Working towards building pico sdk

--- samples/basic/minimal/src/main.c
+++ samples/basic/minimal/src/main.c
@@ -6,11 +6,18 @@
 
 #include <zephyr/kernel.h>
 
+#include <hardware/gpio.h>
+#include <pico/cyw43_arch.h>
+
 int main(void)
 {
 #ifdef CONFIG_SAMPLE_DO_OUTPUT
 	printk("Hello World from minimal!\n");
 #endif
+
+	cyw43_arch_init();
+
+	cyw43_arch_gpio_put(0, true);
 
 	return 0;
 }
